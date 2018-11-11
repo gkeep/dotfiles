0c9bdd90dd8578c1e4b042432a3210c2dd481706
gkeep <gkeep77@outlook.com>
2018-11-11 20:23:07 +0300

    Fixed wallpaper script

--- scripts/dualscreenfixer.py
+++ scripts/dualscreenfixer.py
@@ -1,8 +1,9 @@
 import os
 
 isVGAConnected = open("/sys/class/drm/card0-VGA-1/status", "r")
+isVGAConnected = isVGAConnected.read()
 
-if isVGAConnected == "connected":
+if "connected" in isVGAConnected:
     os.system("bash /home/gkeep/scripts/dualscreen.sh && feh --bg-fill --randomize ~/.wallpaper/*")
 else:
     os.system("feh --bg-fill --randomize ~/.wallpaper/*")
