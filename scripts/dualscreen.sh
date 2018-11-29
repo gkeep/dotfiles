8719e3fd819a25e87d8a516ea67e8b71f0f45c38
gkeep <gkeep77@outlook.com>
2018-11-30 00:46:11 +0300

    Updated i3wm config, uploaded dunst config

--- scripts/dualscreen.sh
+++ scripts/dualscreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x392 --rotate normal
+xrandr --output VGA-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x256 --rotate normal
