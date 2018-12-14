bc3a9695a41a77ca24e882e078bbd80e10055a03
gkeep <gkeep77@outlook.com>
2018-12-14 22:54:09 +0300

    Added configs for mpd & ncmpcpp; updated configs; moved scripts to a separate repo

--- scripts/dualscreen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x256 --rotate normal
