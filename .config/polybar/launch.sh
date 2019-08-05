4f427299479cf3fea98ec7adc0aa07dd1bc11323
gkeep <gkeep77@protonmail.com>
2019-08-05 18:05:40 +0300

    Add polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -9 polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
