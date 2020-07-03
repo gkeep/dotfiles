635481edbf04dfa63c6f6fb7f6097e7d459a0c3f
gkeep <gkeep77@protonmail.com>
2020-07-04 01:11:33 +0300

    Delete old shit

--- scripts/download_theos_ios_sdks.sh
+++
@@ -1,6 +0,0 @@
-# see https://github.com/theos/theos/wiki/Installation-Linux
-curl -LO https://github.com/theos/sdks/archive/master.zip
-TMP=$(mktemp -d)
-unzip master.zip -d $TMP
-mv $TMP/sdks-master/*.sdk $THEOS/sdks
-rm -r master.zip $TMP
